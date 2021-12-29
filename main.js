03ae4aa4d1a0cc452325323d2927d7c9eab2941b
Celina <celina.shen06@gmail.com>
2021-12-28 19:20:31 -0500

    Initialized project

---
+++ main.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const { token } = require('./config.json')
+//https://discord.com/oauth2/authorize?client_id=925541958426972291&scope=bot&permissions=545394785535
