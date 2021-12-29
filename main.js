fd8277cadd23ffdc17986e8863614908c2b892db
Celina <celina.shen06@gmail.com>
2021-12-28 21:11:41 -0500

    bot responds to clibs commands

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
-const { token } = require('./config.json')
+const prefix = '!clibs';
+
+const { token } = require('./config.json');
 //https://discord.com/oauth2/authorize?client_id=925541958426972291&scope=bot&permissions=545394785535
 
+client.once('ready', () => {
+    console.log('Clibs is online')
+});
 
+client.on('messageCreate', message => {
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+
+    console.log(args)
+
+    const command = args[1].toLowerCase();
+
+    if(command === 'store'){
+        message.channel.send('Your message is stored in Clibs!')
+    } else if (command === 'get'){
+        message.channel.send('Your message is retrieved from the Clibs!')
+    } else if (command === 'delete'){
+        message.channel.send('Your message was deleted from Clibs.')
+    }
+});
 
 
 client.login(token)
