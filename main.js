6ab6e5246b0234717ebf8d94c10255f099e42a7c
Celina <celina.shen06@gmail.com>
2021-12-28 20:53:05 -0500

    added config to gitignore

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ const client = new Discord.Client();
 
 const { token } = require('./config.json')
 //https://discord.com/oauth2/authorize?client_id=925541958426972291&scope=bot&permissions=545394785535
+
+
+
+
+client.login(token)
